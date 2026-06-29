ed48e1f20feab489500ccc1a10d3c700387cca80
rafiapanjwani7-afk <rafiapanjwani7@gmail.com>
2026-06-29 12:56:04 +0500

    post app

--- app.js
+++ app.js
@@ -1,75 +1,90 @@
-function signup() {
-    var name = document.getElementById("signName").value;
-    var email = document.getElementById("signEmail").value;
-    var password = document.getElementById("signPassword").value;
-    var users = JSON.parse(localStorage.getItem("userData")) || [];
-    if (name === "" || email === "" || password === "") {
-        Swal.fire("Error", "Please fill in all fields!", "error");
-        return;
-    }
-    if (password.length < 6) {
-        Swal.fire("Error", "Password must be at least 6 characters long!", "error");
-        return;
-    }
-    var exists = false;
-    for (var user of users) {
-        if (user.email === email) {
-            exists = true;
-            break;
-        }
-    }
-    if (exists) {
-        Swal.fire({
-            icon: "warning",
-            title: "Already Exists",
-            text: "This email is already registered!"
-        });
-        return;
-    }
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+const supabase_url = "https://izorczhqnqgwpbjxguoi.supabase.co"
+const publish_key = "sb_publishable_WR1TFRd5ys0ycT4nl228oA_fdYnPxE-"
 
-    var userData = {
-        name: name,
-        email: email,
-        password: password
-    }
-    users.push(userData);
-    localStorage.setItem("userData", JSON.stringify(users));
-    Swal.fire("Success", "Signup successful!", "success");
-    console.log(userData);
+const supabase = createClient(supabase_url, publish_key)
+const signupForm = document.getElementById("signupForm");
 
-    window.location.href = "login.html";
+if (signupForm) {
+    signupForm.addEventListener("submit", signup);
 }
-function login() {
-    var email = document.getElementById("loginEmail").value;
-    var password = document.getElementById("loginPassword").value;
-    var users = JSON.parse(localStorage.getItem("userData")) || [];
-    if (email === "" || password === "") {
+
+async function signup(event) {
+    event.preventDefault();
+
+    console.log("Signup function called");
+
+    const name = document.getElementById("signName").value;
+    const email = document.getElementById("signEmail").value;
+    const password = document.getElementById("signPassword").value;
+
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data: { name }
+        }
+    });
+
+    console.log(data);
+    console.log(error);
+    if (error) {
+        Swal.fire("Error", error.message, "error");
+        return;
+    }
+    Swal.fire({
+        icon: "success",
+        title: "Signup Successful"
+    }).then(() => {
+        window.location.href = "login.html";
+    });
+}
+// console.log(supabase);
+console.log("Signup function called");
+
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+    loginForm.addEventListener("submit", login);
+}
+
+async function login(event) {
+    event.preventDefault();
+    let email = document.getElementById("loginEmail").value;
+    let password = document.getElementById("loginPassword").value;
+
+    if (!email || !password) {
         Swal.fire("Error", "Please fill in all fields!", "error");
         return;
     }
-    var userData = users.find(function (user) {
-        return user.email === email && user.password === password;
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password
     });
-    if (userData) {
-        localStorage.setItem("currentUser", JSON.stringify(userData));
-        Swal.fire({
-            icon: "success",
-            title: "Login Successful!",
-            text: "Welcome back " + userData.name
-        }).then(() => {
+    console.log(data);
+    console.log(error);
+    if (error) {
+        Swal.fire("Error", error.message, "error");
+        return;
+    }
+
+    Swal.fire({
+        icon: "success",
+        title: "Login Successful!"
+    })
+        .then(() => {
             window.location.href = "dashboard.html";
         });
-
-    } else {
-        Swal.fire({
-            icon: "error",
-            title: "Login Failed",
-            text: "Invalid email or password!"
-        });
-    }
 }
-function logout() {
-    localStorage.removeItem("currentUser");
+async function logout() {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+        Swal.fire("Error", error.message, "error");
+        return;
+    }
+
     Swal.fire({
         icon: "success",
         title: "Logged Out",
@@ -78,119 +93,152 @@ function logout() {
         window.location.href = "login.html";
     });
 }
+
+let edited = false;
 var selectedTextColor = "";
 var cardBg = "";
-var time = moment().format("MMMM Do YYYY, h:mm:ss a");
-var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+// var time = moment().format("MMMM Do YYYY, h:mm:ss a");
+// var currentUser = JSON.parse(localStorage.getItem("currentUser"));
 var title = document.getElementById("title");
 var description = document.getElementById("description");
-var editIndex = null;
-var imageInput = document.getElementById("imgInput")
-var previewImg = document.getElementById("previewImg")
-var storedBg = localStorage.getItem("cardBg");
-if (storedBg) {
-    cardBg = storedBg;
-    previewImg.src = cardBg;
-    previewImg.style.display = "block";
+let editIndex = null;
+window.onload = async function () {
+    const postsContainer = document.getElementById("posts");
+
+    try {
+        const { data, error } = await supabase
+            .from('post_app_table')
+            .select("*")
+            .order('id', { ascending: false });
+
+        if (error) {
+            console.log("Supabase Error:", error);
+            return;
+        }
+
+        if (!data) {
+            console.log("No data found");
+            return;
+        }
+
+        data.forEach(post => {
+            postsContainer.innerHTML += `
+<div class="card mb-3">
+
+<div class="card-header d-flex justify-content-between">
+    <span>~post ${post.id}</span>
+
+    <div>
+        <button class="btn  btn-sm"
+        onclick="editPost(event,${post.id},'${post.description}','${post.title}','${post.bg_img}')">
+        <i class="bi bi-pencil-square"></i>
+        </button>
+
+        <button class="btn  btn-sm"
+        onclick="delpost(event,${post.id})">
+         <i class="bi bi-trash"></i>
+        </button>
+    </div>
+</div>
+
+<div class="card-body"
+style="background-image:url('${post.bg_img}');background-size:cover;background-position:center;">
+<h4 style="color:${selectedTextColor}">
+${post.title}
+</h4>
+<p style="color:${selectedTextColor}">
+${post.description}
+</p>
+</div>
+
+</div>
+`;
+        });
+
+    } catch (err) {
+        console.log("Catch Error:", err);
+    }
+};
+async function post() {
+    var title = document.getElementById("title")
+    var description = document.getElementById("description")
+    console.log(title.value, description.value);
+    var posts = document.getElementById("posts")
+    if (title.value.trim() && description.value.trim()) {
+        if (edited) {
+            try {
+                const { data, error } = await supabase
+                    .from('post_app_table')
+                    .update({ title: title.value, description: description.value, bg_img: cardBg })
+                    .eq('id', editIndex)
+                    .select()
+                console.log(data);
+                if (error) {
+                    console.log(error);
+                }
+                edited = false
+                editIndex = null
+                let postBtn = document.getElementById("postBtn")
+                postBtn.innerHTML = "Post"
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        else {
+            try {
+                const { data, error } = await supabase
+                    .from('post_app_table')
+                    .insert({ title: title.value, description: description.value, bg_img: cardBg })
+                    .select()
+                console.log("Post data", data);
+                if (error) console.log(error)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        location.reload()
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Title & description can't be empty!",
+        });
+    }
+    title.value = ""
+    description.value = ""
+}
+function editPost(event, id, desc, title, bg_img) {
+    console.log(title, desc, id);
+    document.getElementById("title").value = title
+    document.getElementById("description").value = desc
+    console.log(title, description);
+   const card = event.target.closest(".card");
+    if (card) card.remove();
+    edited = true;
+    editIndex = id;
+    let postBtn = document.getElementById("postBtn")
+    postBtn.innerHTML = "Update Post"
 }
 
-imageInput.addEventListener("change", function () {
-    var file = imageInput.files[0];
 
-    if (file) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            previewImg.src = e.target.result;
-            previewImg.style.display = "block";
-            cardBg = e.target.result;
-            localStorage.setItem("cardBg", cardBg);
-        };
-        reader.readAsDataURL(file);
-    }
-});
+function addImg(src) {
+    cardBg = src;
 
-function newPost() {
-    var postsContainer = document.getElementById("posts");
-    postsContainer.innerHTML = "";
-    var allposts = JSON.parse(localStorage.getItem("posts")) || [];
-    for (var i = allposts.length - 1; i >= 0; i--) {
-        postsContainer.innerHTML += `
-        <div class="card mb-2">
-            <div class="card-header">
-                ~${allposts[i].name} <br>
-                <small>${allposts[i].time}</small>
-            </div>
-             <div class="card-body" style="background-image:url(${allposts[i].img}); background-size:cover; background-position:center;" class="img-fluid"> 
-                <p style="color:${allposts[i].color};">${allposts[i].title}</p>
-                <small style="color:${allposts[i].color};">${allposts[i].description}</small>
-            </div>
-            <div class="d-flex gap-4 ms-auto mt-1 mb-1">
-                <button class="btn editBtn  p-1" onclick="editBtn(${i})">Edit</button>
-                <button class="btn delBtn  p-1" onclick="delBtn(${i})">Delete</button>
-            </div>
-        </div>
-        `;
-    }
+    const images = document.querySelectorAll(".bgImg");
+
+    images.forEach((img) => {
+        img.classList.remove("addImg");
+
+        if (img.getAttribute("src") === src) {
+            img.classList.add("addImg");
+        }
+    });
 }
-newPost();
-function post() {
-    var postTitle = document.getElementById("title");
-    var postDesc = document.getElementById("description");
+async function delpost(event, id) {
 
-    var allPosts = JSON.parse(localStorage.getItem("posts")) || [];
-
-    if (!postTitle.value.trim() || !postDesc.value.trim()) {
-        Swal.fire("Error", "Title & description required!", "error");
-        return;
-    }
-    if (editIndex !== null) {
-        allPosts[editIndex].title = postTitle.value;
-        allPosts[editIndex].description = postDesc.value;
-        allPosts[editIndex].img = cardBg;
-        allPosts[editIndex].color = selectedTextColor;
-        editIndex = null;
-        Swal.fire("Updated!", "Post updated successfully", "success");
-    }
-    else {
-
-        var postObj = {
-            title: postTitle.value,
-            description: postDesc.value,
-            name: currentUser.name,
-            time: moment().format("MMMM Do YYYY, h:mm:ss a"),
-            img: cardBg,
-            color: selectedTextColor,
-            likes: 0
-        };
-
-        allPosts.push(postObj);
-    }
-
-    localStorage.setItem("posts", JSON.stringify(allPosts));
-    postTitle.value = "";
-    postDesc.value = "";
-    selectedTextColor = "";
-    cardBg = "";
-    previewImg.style.display = "none";
-    previewImg.src = "";
-    imageInput.value = "";
-    newPost();
-}
-function editBtn(index) {
-    var allPosts = JSON.parse(localStorage.getItem("posts")) || [];
-
-    document.getElementById("title").value = allPosts[index].title;
-    document.getElementById("description").value = allPosts[index].description;
-     cardBg = allPosts[index].img;
-    if (cardBg) {
-        previewImg.src = cardBg;
-      previewImg.style.display = "block";}
-
-    editIndex = index;
-    Swal.fire("Edit Mode", "Now update your post", "info");
-}
-function delBtn(index) {
-    Swal.fire({
+    const result = await Swal.fire({
         title: "Are you sure?",
         text: "This post will be deleted permanently!",
         icon: "warning",
@@ -198,33 +246,32 @@ function delBtn(index) {
         confirmButtonColor: "#d33",
         cancelButtonColor: "#3085d6",
         confirmButtonText: "Yes, delete it!"
-    }).then((result) => {
-        if (result.isConfirmed) {
-
-            var allPosts = JSON.parse(localStorage.getItem("posts")) || [];
-            allPosts.splice(index, 1);
-            localStorage.setItem("posts", JSON.stringify(allPosts));
-            newPost();
-
-            Swal.fire(
-                "Deleted!",
-                "Your post has been deleted.",
-                "success"
-            );
-        }
     });
 
-}
-// IMAGE
-function addImg(src) {
-    cardBg = src;
-    var bgImg = document.getElementsByClassName("bgImg");
-    for (var i = 0; i < bgImg.length; i++) {
-        console.log(bgImg[i].className);
-        bgImg[i].className = "bgImg"
+    if (!result.isConfirmed) return;
+
+    try {
+        const { data, error } = await supabase
+            .from('post_app_table')
+            .delete()
+            .eq('id', id);
+
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        Swal.fire("Deleted!", "Your post has been deleted.", "success");
+
+       const card = event.target.closest(".card");
+    if (card) card.remove();
+
+    } catch (error) {
+        console.log(error);
     }
-    event.target.classList.add("addImg");
 }
+
+//color
 function applycolor(element) {
     var colorbox = document.getElementsByClassName('colorbox');
     for (var i = 0; i < colorbox.length; i++) {
@@ -233,3 +280,9 @@ function applycolor(element) {
     element.classList.add('selected');
     selectedTextColor = element.style.backgroundColor;
 }
+window.logout = logout;
+window.post = post;
+window.addImg = addImg;
+window.applycolor = applycolor;
+window.editPost = editPost;
+window.delpost = delpost;
